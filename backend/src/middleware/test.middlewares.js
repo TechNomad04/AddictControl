ae6485417b4a3be2c6fc057cc700f4a07ea85d89
TechNomad04 <agarwalsamiksha88@gmail.com>
2025-12-19 16:35:33 +0530

    Added submission for questions

--- backend/src/middleware/test.middlewares.js
+++ backend/src/middleware/test.middlewares.js
@@ -1,16 +1,16 @@
 const {generate} = require('../utils/generateques.util')
 const {Test} = require('../models/Test.model')
-const {normalize} = require('../utils/testcheck.utils')
+const {scoreanswer} = require('../utils/testcheck.utils')
 
 const givetest = async(req, res) => {
     try {
-        const alcohol_id = req.body.id;
-        const questest = new Test({alcohol_id});
-        await questest.save()
+        // const alcohol_id = req.body.id;
+        // const questest = new Test({alcohol_id});
+        // await questest.save()
         const test = await generate();
         if(!test.questions || test.questions.length !== 5)
             return res.status(401).json({status:false, message:"Gemini error"})
-        return res.status(200).json({status: true, test, test_id:questest._id});
+        return res.status(200).json({status: true, test});
     } catch (err) {
         console.log(err)
         return res.status(500).json({status:false, message: "Internal server error"})
@@ -19,8 +19,9 @@ const givetest = async(req, res) => {
 
 const submitanswer = async(req, res) => {
     try {
-        const {answer, expected_answer, question_category} = req.body;
-        const nanswer = normalize(answer)
+        const {answer, question} = req.body;
+        const nanswer = await scoreanswer(question, answer)
+        return res.status(200).json({status: true, nanswer})
 
     } catch (err) {
         console.log(err)
@@ -29,5 +30,6 @@ const submitanswer = async(req, res) => {
 }
 
 module.exports = {
-    givetest
+    givetest,
+    submitanswer
 }
--- backend/src/routes/test.routes.js
+++ backend/src/routes/test.routes.js
@@ -1,8 +1,9 @@
-const {givetest} = require('../middleware/test.middlewares')
+const {givetest, submitanswer} = require('../middleware/test.middlewares')
 const express = require('express')
 
 const router = express.Router()
 
 router.get('/questions', givetest);
+router.post('/submit', submitanswer);
 
 module.exports = router
